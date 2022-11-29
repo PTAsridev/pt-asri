2c1c621546402cf3132b9b9c93d55c66c85535a8
Mekel Verellino <verellino@gmail.com>
2022-11-29 11:37:56 +0700

    update hero bg

--- components/global/MainHero.vue
+++ components/global/MainHero.vue
@@ -1,22 +1,22 @@
 <template>
 <div class="isolate bg-white">
   <main>
-    <!-- <div class="relative px-6 lg:px-8 bg-cover bg-center" :style="{ backgroundImage: `url(${backgroundUrl})` }"> -->
-    <div class="relative px-6 lg:px-8 bg-cover bg-center">
-      <div class="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-20">
+    <div class="relative px-6 lg:px-8 bg-cover bg-bottom bg-blend-overlay" :style="{ backgroundImage: `url(${backgroundUrl})` }">
+    <!-- <div class="relative px-6 lg:px-8 bg-cover bg-center"> -->
+      <div class="mx-auto max-w-3xl pt-20 pb-32 mt-28 sm:pt-32 sm:pb-20">
         <div>
           <div>
-            <h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-8xl">A partner for success</h1>
-            <p class="mt-6 text-lg leading-8 text-gray-600 sm:text-center">PT. Asri is a supplier and service company, providing nationwide products and services to our customers in the hospitality, catering, commercial and residential industries. We offer a wide range of quality products and services that are specifically designed for use in demanding environments.</p>
+            <h1 class="text-4xl font-bold tracking-tight text-white sm:text-center sm:text-8xl">A partner for success</h1>
+            <p class="mt-6 text-lg leading-8 text-gray-100 sm:text-center">PT. Asri is a supplier and service company, providing nationwide products and services to our customers in the hospitality, catering, commercial and residential industries. We offer a wide range of quality products and services that are specifically designed for use in demanding environments.</p>
             <div class="mt-8 flex gap-x-4 sm:justify-center">
-              <a href="#" class="inline-block rounded-lg px-8 py-4 text-base font-semibold leading-7 text-white shadow-sm main-button">
-                Get started
+              <a href="https://wa.me/+6287733733030" class="inline-block rounded-lg px-8 py-4 text-base font-semibold leading-7 text-white shadow-sm main-button">
+                Schedule a Call
                 <span class="text-indigo-200" aria-hidden="true">&rarr;</span>
               </a>
-              <a href="#" class="inline-block rounded-lg px-8 py-4 text-base font-semibold leading-7 text-orange text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Live demo
-                <span class="text-gray-400" aria-hidden="true">&rarr;</span>
-              </a>
+              <nuxt-link to="/services" class="inline-block rounded-lg px-8 py-4 text-base font-semibold leading-7 text-orange text-gray-100 ring-1 ring-gray-100/10 hover:ring-gray-100/20">
+                Our Services
+                <span class="text-gray-100" aria-hidden="true">&rarr;</span>
+              </nuxt-link>
             </div>
           </div>
           <div class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]">
@@ -44,7 +44,7 @@
 </style>
 
 <script>
-import backgroundUrl from '~/assets/images/building/7e961055-c54c-4403-b26a-982519aae6ee.jpg'
+import backgroundUrl from '~/assets/images/2561afbd-2b78-4742-bf80-5a6caeceac39.jpg'
 
 export default {
   data() {
